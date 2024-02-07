4a8bd451ca54fb189ca2e528db9d89d570d2e1ad
HiyoshiTurio <134475619+HiyoshiTurio@users.noreply.github.com>
2024-02-07 14:39:28 +0900

    [add]skyboxの追加、島のプレハブの追加 [update]プレイヤーの移動をvelocityから、addForceに変更

--- Assets/Scripts/MoveAddforce.cs
+++ Assets/Scripts/MoveAddforce.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MoveAddforce : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class MoveAddforce : MonoBehaviour, IPause
 {
-    [SerializeField] float _movePower = 3;
-    [SerializeField] Animator _animator;
-    Rigidbody _rb = default;
+    [SerializeField] float _movePower = 3f;
+    [SerializeField] float _jumpPower = 1f;
+    [SerializeField] float _LimitSpeed = 1f;
+    [SerializeField] float _stuckTime = 1f;
+    [Tooltip("着地時の硬直時間")]
+    [SerializeField] float _waitForIsGround = 1f;
+    [SerializeField] Animator _anim;
+    [SerializeField]bool _cursorVisible = false;
+    float _timer;
+    float _speed;
+    float _fallSpeed;
+    float _savedAnimSpeed;
+    
     bool _isGround = false;
+    Vector3 _velocity;
+    Vector3 _angularVelocity;
+    Vector3 _reSponePos;
+    Rigidbody _rb = default;
+
     /// <summary>入力された方向の XZ 平面でのベクトル</summary>
     Vector3 _dir;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+        if(!_cursorVisible) Cursor.visible = false;
+        _reSponePos = this.transform.position;
     }
 
     void Update()
@@ -36,15 +54,38 @@ public class MoveAddforce : MonoBehaviour
         {
             this.transform.forward = forward;
         }
-        float _speed = forward.magnitude;
 
-        float _fallSpeed = _rb.velocity.y;
-        _animator.SetFloat("Speed", _speed);
-        _animator.SetFloat("FallSpeed", _fallSpeed);
-        _animator.SetBool("IsGround", _isGround);
+        if (Input.GetButtonDown("Jump") && _isGround && _timer > _waitForIsGround)
+        {
+            Vector3 velocity = _rb.velocity;
+            velocity.y = _jumpPower;
+            _rb.velocity = velocity;
+            _timer = 0;
+        }
+
+        _speed = forward.magnitude;
+        _fallSpeed = _rb.velocity.y;
+        if (_speed > _LimitSpeed)
+        {
+            _rb.velocity = new Vector3(_rb.velocity.x / 1.1f, _rb.velocity.y, _rb.velocity.z / 1.1f);
+        }
+        _anim.SetFloat("Speed", _speed);
+        _anim.SetFloat("UpSpeed", _fallSpeed);
+        _anim.SetBool("IsGround", _isGround);
+
+        if (_isGround && _timer < _waitForIsGround)
+        {
+            _timer += Time.deltaTime;
+        }
+
+        if (this.transform.position.y < -10)
+        {
+            _rb.velocity = new Vector3(0, 0, 0);
+            this.transform.position = _reSponePos;
+        }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         _dir.y = 0;
         _rb.AddForce(_dir.normalized * _movePower, ForceMode.Force);
@@ -56,6 +97,10 @@ public class MoveAddforce : MonoBehaviour
         {
             _isGround = true;
         }
+        if (collider.gameObject.tag == "Goal")
+        {
+            SceneManager.LoadScene("GoalScene");
+        }
     }
     void OnTriggerExit(Collider collider)
     {
@@ -63,5 +108,40 @@ public class MoveAddforce : MonoBehaviour
         {
             _isGround = false;
         }
+    }
+
+    void OnApplicationQuit()
+    {
+        Cursor.visible = true;
+    }
+
+    public void Pause()
+    {
+        Debug.Log("Pause");
+        // 速度・回転を保存し、Rigidbody を停止する
+        _angularVelocity = _rb.angularVelocity;
+        _velocity = _rb.velocity;
+        _rb.isKinematic = true;
+        _rb.Sleep();
+        
+        Cursor.visible = true;
+        // アニメーションを止める（再生速度を0にする）
+        _savedAnimSpeed = _anim.speed;
+        _anim.speed = 0;
+    }
+
+    public void Resume()
+    {
+        Debug.Log("Resume");
+        Debug.Log(_velocity);
+        // Rigidbody の活動を再開し、保存しておいた速度・回転を戻す
+        _rb.WakeUp();
+        _rb.isKinematic = false;
+        _rb.angularVelocity = _angularVelocity;
+        _rb.AddForce(_velocity, ForceMode.Impulse);
+
+        Cursor.visible = false;
+        //
+        _anim.speed = _savedAnimSpeed;
     }
 }
